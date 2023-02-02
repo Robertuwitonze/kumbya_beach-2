a5140dd08aad56fd34283c3340d14473db577c9f
UWITONZE Robert <93436610+Robertuwitonze@users.noreply.github.com>
2023-02-02 11:52:21 +0000

    creating room model and registering it

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,1 +1,6 @@
 from django.contrib import admin
+from .model import Room
+
+@admin.register(Room)
+class Room(admin.ModelAdmin):
+    list_display = ['room_number','pictures', 'number_of_bed', 'cost', 'description','status'] 
--- app/core/models.py
+++ app/core/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+
+# creating room model
+class Room(models.Model):
+    room_number = models.CharField(max_length=250, null= True)
+    pictures = models.FileField(upload_to='images/')
+    number_of_bed = models.CharField(max_length=250, default=1)
+    cost = models.CharField(max_length=250)
+    description = models.TextField()
+    status = models.CharField(max_length=250, default='')
